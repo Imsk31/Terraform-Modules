316aee087373a371d9a1ad61d970c574c62aec14
Imsk31 <shubhamkalekar8805@gmail.com>
2024-09-21 16:15:49 +0530

    changes

--- vpc-module/main.tf
+++ vpc-module/main.tf
@@ -27,8 +27,8 @@ resource "aws_subnet" "public-subnet" {
 }
 
 resource "aws_route_table" "terraform-vpc-pub-sub-rt" {
-  vpc_id = aws_vpc.terraform_vpc.id
   count = length(var.public-subnet-cidr)
+  vpc_id = aws_vpc.terraform_vpc.id
 
   tags = {
     value = "public subnet rt"
@@ -36,11 +36,15 @@ resource "aws_route_table" "terraform-vpc-pub-sub-rt" {
 }
 
 resource "aws_route_table_association" "terraform-vpc-pub-rt-assocication" {
-  route_table_id = aws_route_table.terraform-vpc-pub-sub-rt.id
-  subnet_id = aws_subnet.public-subnet.0.id
   count = length(var.public-subnet-cidr)
-}
+  route_table_id = aws_route_table.terraform-vpc-pub-sub-rt.id
+  subnet_id = aws_route_table.terraform-vpc-pub-sub-rt[count.index].id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
 
 
 resource "aws_subnet" "private-subnet" {
@@ -56,7 +60,7 @@ tags = {
 }
 
 resource "aws_eip" "nat-eip" {
-  domain = vpc
+  vpc = true
   network_border_group = var.aws_region 
 }
 
@@ -70,6 +74,22 @@ tags = {
 }
 }
 
+resource "aws_route_table" "pri-rt" {
+  vpc_id = aws_vpc.terraform_vpc.id
+  count = length(var.private-subnet-cidr)
+
+  tags = {
+    value = "private subnet rt"
+  }
+  
+}
+
+resource "aws_route_table_association" "terraform-vpc-pri-rt-association" {
+  count = length(var.private-subnet-cidr)
+  route_table_id = aws_route_table.pri-rt.id
+  subnet_id = aws_subnet.private-subnet[count.index].id
+
+}
 
 
 
