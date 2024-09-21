79561c547c0fdd2c64ac5869b93c738ef2a7cefb
Imsk31 <shubhamkalekar8805@gmail.com>
2024-09-21 18:24:38 +0530

    changes

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 module "vpc" {
-  source = "vpc-module"
-  vpc_cidr = var.vpc_cid
-  instance_tenancy = var.instance_tenancy
-  vpc_name = var.vpc_name
-  igw_name = var.igw_name
-  public_subnet_cidr = var.public-subnet-cidr
+  source              = "./vpc-module"
+  vpc_cidr            = var.vpc_cidr
+  instance_tenancy    = var.instance_tenancy
+  vpc_name            = var.vpc_name
+  igw_name            = var.igw_name
+  public_subnet_cidr  = var.public_subnet_cidr
   private_subnet_cidr = var.private_subnet_cidr
-  subnet_az = var.subnet-az
+  subnet_az           = var.subnet_az
+  aws_region          = var.aws_region
 }
